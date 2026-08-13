e48a962e57ab55b69e3372540865aac7dbf0175f
Tano <estanissanta@gmail.com>
2026-08-13 16:34:05 -0300

    fix no rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-const siteUrl = process.env.SITE_URL || 'https://oktanon.github.io/svencoop-maps';
-
 export default defineConfig({
-  site: siteUrl,
+  site: 'https://oktanon.github.io',
+  base: '/svencoop-maps/',
   integrations: [react(), sitemap()],
 });
